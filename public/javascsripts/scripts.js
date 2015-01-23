94d368a2ec8d5af892c994a22814aeb73612f856
reihned <reihned@gmail.com>
2015-01-23 12:21:40 -0500

    more setup

---
+++ public/javascsripts/scripts.js
@@ -0,0 +1,6 @@
+$(
+  function(){
+    var categories = new ContactList.Collections.Category();
+    var categoriesView = new ContactList.Views.Categories();
+  }//function to run when ready
+);//jquery ready
--- server.rb
+++ server.rb
@@ -41,7 +41,7 @@ end
 delete("/categories/:id") do
   category = Category.find(params[:id])
   category.destroy
-  
+
   category.to_json
 end
 
@@ -50,7 +50,7 @@ get("/contacts") do
 end
 
 get("/contacts/:id") do
-  Contact.find_by(params[:id]).to_json
+  Contact.find_by(id: params[:id]).to_json
 end
 
 post("/contacts") do
