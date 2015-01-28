e9fcd611205119846cb83ae3ed26de019347180f
reihned <reihned@gmail.com>
2015-01-27 22:36:43 -0500

    HA! fixed div select instead of form select, also form moved from index to scripts

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,17 @@
 var ContactList = ContactList || { Models: {}, Collections: {}, Views: {} };
 
-
+var newContactForm = [
+  "<h3>New Contact</h3>",
+  "<form id='contactNew'>",
+    "<p>Name:<input type='text' name='name'></input></p>",
+    "<p>Age:<input type='text' name='age'></input></p>",
+    "<p>Address:<input type='text' name='address'></input></p>",
+    "<p>Phone Number:<input type='text' name='phone_number'></input></p>",
+    "<p>Picture Url:<input type='text' name='picture'></input></p>",
+    "<p>Category:<select id='contactCat' name='category_id'></select></p>",
+    "<input id='contactSubmit' value='submit' type='submit'></input>",
+  "</form>"
+].join("");
 
 $(
   function(){
@@ -10,15 +21,19 @@ $(
       el: $("#main")
     });
 
+
     categories.fetch({reset: true});
 
-    var $formMain = $('form#formMain');
+    var $divFormMain = $('div#formMain');
+    var $formMain = $(newContactForm);
+    $formMain.appendTo($divFormMain);
     $formMain.submit(function(e){
       e.preventDefault();
       var newContact = new ContactList.Models.Contact();
       console.log( $( this ).serialize() );
       return false;
     });
+
 
   }//function to run when ready
 );//jquery ready
